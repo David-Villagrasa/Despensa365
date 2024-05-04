8c09284e4942b04af249c34bf37e974febd92150
David Villagrasa <107490517+David-Villagrasa@users.noreply.github.com>
2024-05-04 20:12:25 +0200

    MainActivity.java started

--- app/src/main/java/com/example/despensa365/MainActivity.java
+++ app/src/main/java/com/example/despensa365/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.despensa365;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin, btnRegister, btnLogout, btnManWeek, btnManRec, btnManPantry, btnManToBuy;
+    TextView tvWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnLogin=findViewById(R.id.btnLogIn);
+        btnRegister=findViewById(R.id.btnRegister);
+        btnLogout=findViewById(R.id.btnLogOut);
+        btnManWeek=findViewById(R.id.btnManageWeek);
+        btnManRec=findViewById(R.id.btnManageRecipes);
+        btnManPantry=findViewById(R.id.btnManagePantry);
+        btnManToBuy=findViewById(R.id.btnManageList);
+        tvWelcome=findViewById(R.id.tvWelcome);
+
     }
+
+    public void clickLogin(View v) {
+
+    }
+
+    public void clickRegister(View v) {
+
+    }
+
+    public void clickLogout(View v) {
+
+    }
+
+    public void clickWeek(View v) {
+
+    }
+
+    public void clickRecipes(View v) {
+
+    }
+
+    public void clickPantry(View v) {
+
+    }
+
+    public void clickToBuy(View v) {
+
+    }
+
+
 }
